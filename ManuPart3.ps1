ea04d634d2360fd287f9467f633792d1c7aa4c25
Jacob Chappell <thebloodyend@gmail.com>
2020-10-21 21:22:52 -0500

    Update ManuPart3.ps1

--- ManuPart3.ps1
+++ ManuPart3.ps1
@@ -1,19 +1,30 @@
-﻿Write-Output "Third part:"
-$prevFile = "C:\Users\Family\Desktop\TempDCT\PracScript.xml"
-Write-Output $prevFile
-$newFile = "C:\Users\Family\Desktop\TempDCT\DiffPracScript.xml"
-Write-Output $newFile.
-if (Compare-Object -ReferenceObject  $(Get-Content $prevFile) -DifferenceObject $(Get-Content $newFile)) {
+Write-Output "Third part:"
+$clientPath = Read-Host -Prompt "Enter client name: (ex. Three)"
+$clientPath = 'C:\Users\Family\Desktop\TempDCT\' + $clientPath
+Write-Host $clientPath
+$fpathName = Read-Host -Prompt "Enter file path for the first file: (ex. PracScript.xml)"
+$fpathName = $clientPath + '\' + $fpathName
+Write-Host $fpathName
+$spathName = Read-Host -Prompt "Enter file path for the second file: (ex. DiffPracScript.xml)"
+$spathName = $clientPath + '\' + $spathName
+Write-Host $spathName
+if (Compare-Object -ReferenceObject  $(Get-Content $fpathName) -DifferenceObject $(Get-Content $spathName)) {
     "The files had different contents"
-    } else {
-        "The files were the same"
-    }
+} else {
+     "The files were the same"
+}
+
 Write-Output " "
-$prevFile = "C:\Users\Family\Desktop\TempDCT\PracScript.xml"
-Write-Output $prevFile
-$newFile = "C:\Users\Family\Desktop\TempDCT\EmptyPracScript.xml"
-Write-Output $newFile.
-if (Compare-Object -ReferenceObject  $(Get-Content $prevFile) -DifferenceObject $(Get-Content $newFile)) {
+$clientPath = Read-Host -Prompt "Enter client name: (ex. Three)"
+$clientPath = 'C:\Users\Family\Desktop\TempDCT\' + $clientPath
+Write-Host $clientPath
+$fpathName = Read-Host -Prompt "Enter file path for the first file: (ex. PracScript.xml)"
+$fpathName = $clientPath + '\' + $fpathName
+Write-Host $fpathName
+$spathName = Read-Host -Prompt "Enter file path for the second file: (ex. EmptyPracScript.xml)"
+$spathName = $clientPath + '\' +  $spathName
+Write-Host $spathName
+if (Compare-Object -ReferenceObject  $(Get-Content $fpathName) -DifferenceObject $(Get-Content $spathName)) {
     "The files had different contents"
 } else {
         "The files were the same"
