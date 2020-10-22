d71f07699470be19c154db58cbe928a39abf7332
Jacob Chappell <thebloodyend@gmail.com>
2020-10-21 21:23:13 -0500

    Update ManuPart4.ps1

--- ManuPart4.ps1
+++ ManuPart4.ps1
@@ -1,40 +1,98 @@
-﻿Write-Output "Fourth part:"
+Write-Output "Fourth part:"
 #lines about checking if table is present
-$clientLOB = Read-Host -Prompt "Enter LOB: "
-$tableLOB = '<table id="Manuscript' +$clientLOB
-$tableLine = Get-ChildItem -Path C:\Users\Family\Desktop\TempDCT\PracTable.xml | Select-String -Pattern $tableLOB
+$clientLOB = Read-Host -Prompt "Enter LOB "
+$tableLOB = '<table id="Manuscripts' + $clientLOB
+Write-Host $tableLOB
+$clientPath = Read-Host -Prompt "Enter client name to verfiy there is a table or not (ex. Five)"
+$clientPath = 'C:\Users\Family\Desktop\TempDCT\' + $clientPath
+Write-Host $clientPath
+#$tableFileName = Read-Host -Prompt "Enter the client version (ex. 10_41_0_0)"
+#$tableFilePath = $clientPath + '\' + $tableFileName  + '.xml'
+$max = ""
+Get-ChildItem -Path $clientPath | ForEach-Object {
+    if ($max -lt $_.Name) {
+        $max = $_.Name
+        Write-Host $max
+    }
+}
+$tableFilePath = $clientPath + "\" + $max
+Write-Host $tableFilePath
+#$ftableFileName = Read-Host -Prompt "Enter the first client version: (ex. PracTable.xml)"
+#$ftableFilePath = $clientPath + '\' + $ftableFileName
+#Write-Host $ftableFilePath
+#$stableFileName = Read-Host -Prompt "Enter the second client version: (ex. PracNewTable.xml)"
+#$stableFilePath = $clientPath + '\' + $stableFileName
+#Write-Host $stableFilePath
+#try {
+$tableLine = Get-ChildItem -Path $tableFilePath | Select-String -Pattern $tableLOB -Quiet
+Write-Host $tableLine
+#$ftableLine = Get-ChildItem -Path $ftableFilePath -ErrorAction stop | Select-String ($tableLOB + '*') -Quiet
+#Write-Host $ftableLine
+#$stableLine = Get-ChildItem -Path $stableFilePath -ErrorAction stop | Select-String ($tableLOB + '*') -Quiet
+#Write-Host $stableLine
+#} catch [System.Exception] {
+#Write-Host "An error occured "
+#Write-Host $_
+#}
+#$tableLine = Get-ChildItem -Path C:\Users\Family\Desktop\TempDCT\PracTable.xml | Select-String -Pattern $tableLOB
 #if there or not {
-$compareString = ""
-$XMLFile = 'C:\Users\Family\Desktop\TempDCT\PracTable.xml'
-[XML]$tableData = Get-Content $XMLFile
-foreach($empDetail in $tableData.table.data.row){
-    #Write-Output $empDetail.value
-    $compareString += $empDetail.value
-}
+$max = $max.Replace(".xml", "")
+Write-Output $max
 Write-Output " "
-$compareString2 = ""
-$XMLFile2 = 'C:\Users\Family\Desktop\TempDCT\PracNewTable.xml'
-[XML]$newTableData = Get-Content $XMLFile2
-foreach($empDetail2 in $newTableData.table.data.row){
-    #Write-Output $empDetail2.value
-    $compareString2 += $empDetail2.value
-}
-$compareString.Equals($compareString2)
-
+if ($tableLine) {
 $compareString = ""
-$XMLFile = 'C:\Users\Family\Desktop\TempDCT\PracTable.xml'
-[XML]$tableData = Get-Content $XMLFile
-foreach($empDetail in $tableData.table.data.row){
+$ifTrue = "False"
+#$XMLFile = 'C:\Users\Family\Desktop\TempDCT\PracTable.xml'
+#$XMLFile = $ftableLine
+[XML]$tableData = Get-Content $tableFilePath
+foreach($empDetail in $tableData.table.data.row) {
     #Write-Output $empDetail.value
-    $compareString += $empDetail.value
+    $compareString = $empDetail.value
+    #Write-Host $empDetail.value
+    if ($empDetail.value.Contains($max)) {
+        $ifTrue = "True"
+        break
+    }
 }
-Write-Output " "
-
-$compareString2 = ""
-$XMLFile2 = 'C:\Users\Family\Desktop\TempDCT\CopyPracTable.xml'
-[XML]$newTableData = Get-Content $XMLFile2
-foreach($empDetail2 in $newTableData.table.data.row){
+if ($ifTrue -eq "True") {
+    Write-Host "Manuscript table was successfully updated"
+} else {
+    Write-Host "Manuscript table failed to update successfully"
+}
+#$compareString2 = ""
+#$XMLFile2 = 'C:\Users\Family\Desktop\TempDCT\PracNewTable.xml'
+#$XMLFile2 = $stableLine
+#[XML]$newTableData = Get-Content $XMLFile2
+#foreach($empDetail2 in $newTableData.table.data.row){
     #Write-Output $empDetail2.value
-    $compareString2 += $empDetail2.value
+#    $compareString2 += $empDetail2.value
+#}
+#$compareString.Equals(('*' + $clientLOB + '*' + $tableFileName + ;'*'))
+} else {
+    Write-Host "No table present"
 }
-$compareString.Equals($compareString2)
+
+#Write-Output " "
+#if ($tableLine -eq $true) {
+#$compareString = ""
+#$XMLFile = 'C:\Users\Family\Desktop\TempDCT\PracTable.xml'
+#$XMLFile = $ftableLine
+#[XML]$tableData = Get-Content $XMLFile
+#foreach($empDetail in $tableData.table.data.row){
+    #Write-Output $empDetail.value
+#    $compareString += $empDetail.value
+#}
+#Write-Output " "
+
+#$compareString2 = ""
+#$XMLFile2 = 'C:\Users\Family\Desktop\TempDCT\CopyPracTable.xml'
+#$XMLFile2 = $stableLine
+#[XML]$newTableData = Get-Content $XMLFile2
+#foreach($empDetail2 in $newTableData.table.data.row){
+    #Write-Output $empDetail2.value
+#    $compareString2 += $empDetail2.value
+#}
+#$compareString.Equals($compareString2)
+#} else {
+#    Write-Host "No table present"
+#}
