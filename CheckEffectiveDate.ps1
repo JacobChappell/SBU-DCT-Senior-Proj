f6c376c9cd616e0fb5d129d490fa6461772a84e7
cbdublu <42583139+cbdublu@users.noreply.github.com>
2020-11-04 00:31:22 -0600

    Update Compare Tool with more descriptive errors

--- CheckEffectiveDate.ps1
+++ CheckEffectiveDate.ps1
@@ -1,4 +1,5 @@
-﻿        $stateArray = @('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS',
+﻿ $ErrorActionPreference = 'SilentlyContinue'
+        $stateArray = @('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS',
                         'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY',
                         'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY')
 
@@ -15,10 +16,8 @@
             }
                 
             else{
-                Write-Error "Path Check Failed"
-
-            } 
-
+                Write-Error "Path Check Failed in file " $arrayPosition
+            }
                 
             if ( $File -ne $null -and $File2 -ne $null){
                 $date = $File.SelectSingleNode('//effectiveDateNew').'#text' -as [datetime]
@@ -28,17 +27,17 @@
                 Write-Output $date2
 
                 if ($date -lt $date2){
-                    Write-Output "Effective Date Valid"
+                    Write-Host -ForegroundColor Green "Effective Date Valid in file $arrayPosition" `n
                 }
 
                 else{
-                    Write-Output "Invaild Effective Date"
+                    Write-Host -ForegroundColor Red "Invaild Effective Date in file $arrayPosition" `n
                 }
 
             }
                     
             else{
-                Write-Output "Invalid File"
+                Write-Error "Invalid File"
             }
 
             $File = $null
