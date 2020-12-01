7cea425ae6ae282d1ce4ccf9cf7a6269752f0bcc
EzekialPage <39393643+EzekialPage@users.noreply.github.com>
2020-11-30 19:05:47 -0600

    Update ManuPart1.ps1

--- ManuPart1.ps1
+++ ManuPart1.ps1
@@ -1,40 +1,63 @@
 #Validate encoding of file based on BOM (Byte Order Mark)
 #Based on code from https://gist.github.com/jpoehls/2406504
 Write-Output "First part"
-function Get-FileEncoding {
-  [CmdletBinding()] 
-  Param (
-    [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)] 
-    [string]$Path
-    )
-    #Write-Host Bytes: $byte[0] $byte[1] $byte[2] $byte[3]
-    [byte[]]$byte = get-content -Encoding byte -ReadCount 4 -TotalCount 4 -Path $Path
+
+#input from employee of state + version number. only wanting to run say 9/20 state folders
+$clientName = Read-Host -Prompt "Enter client name (ex. TESTCLIENT) "
+$clientLOBName = Read-Host -Prompt "Enter client's LOB folder (ex. property) "
+$clientPath = 'C:\Users\ebpag\Desktop\DuckCreek\' + $clientName + '\'+ $clientLOBName
+
+#function to check file for UTF-8 encoding
+function getEncoding($fileName){
+    $ret = ""
+     [byte[]]$byte = get-content -Encoding byte -ReadCount 4 -TotalCount 4 -Path $fileName
     # EF BB BF (UTF8)
      if ( $byte[0] -eq 0xef -and $byte[1] -eq 0xbb -and $byte[2] -eq 0xbf ) {
-        Write-Output 'UTF-8 with BOM'
+        $ret =  'UTF-8 with BOM'
     } else { 
-        Write-Output 'Wrong encoding or no BOM' 
+        $ret = 'Wrong encoding or no BOM'
+    }
+    return $ret
+}
+
+#Determine path of newest version
+function newestPath($clPath){
+    $newestPath = ""
+    Get-ChildItem -Path $clPath | ForEach{
+        if($_.Name -gt $newestPath){
+            $newestPath = $_.FullName
+        }
+    }
+    return $newestPath
+}
+
+$flag = 0
+#Iterate through folders in LOB    
+Get-ChildItem -Path $clientPath | ForEach{
+    $old = $_.FullName+"\*.xml"
+    $maxPath = newestPath($old)
+
+    #store output variable names
+    $stateName = $_.Name
+    $fileInfo = Get-ChildItem -Path $maxPath
+    $fileName = $fileInfo.Name
+    
+    #store file encoding result in variable
+    $encodingCheck = getEncoding($maxPath)
+
+    #check for incorrect encoding
+    if ($encodingCheck -ne 'UTF-8 with BOM') {
+        #check for previous errors
+        if($flag -eq 0){
+            Write-Host "The following files have incorrect encoding:" -ForegroundColor Red
+            $flag++
+        }
+        Write-Host 'Folder: ' $stateName
+        Write-Host $fileName
+        Write-Host
     }
 }
-$clientName = Read-Host -Prompt "Enter client name (ex. Five) "
-$clientPath = 'C:\Users\Family\Desktop\TempDCT\' + $clientName + '\*'
-Write-Host $clientPath
-try {
-     $max = ""
-     $maxPath = ""
-     Get-ChildItem -Path ($clientPath) | ForEach-Object {
-        $stateName = $_.Name
-        Get-ChildItem -Path $_.FullName | ForEach-Object {
-            if ($max -lt $_.Name) {
-                $maxPath = $_.FullName
-            }
-        }
-        Get-ChildItem -Path $maxPath -ErrorAction stop | Select Name, @{n='Encoding';e={Get-FileEncoding $_.FullName}}, @{n='State';e={$stateName}}
-     }
-     #$maxPath = $clientPath + $max
-     #Write-Host $maxPath
-     #Get-ChildItem -Path $maxPath -ErrorAction stop | Select Name, @{n='Encoding';e={Get-FileEncoding $_.FullName}}, @{n='State';e={$_.FullName}}
-} catch [System.Exception] {
-     Write-Host "An error occured "
-     Write-Host $_
+#check to see if there were no errors
+if($flag -eq 0){
+        Write-Host "All files have UTF-8 with BOM"
 }
