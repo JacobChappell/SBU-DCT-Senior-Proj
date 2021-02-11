7dde957819b465552fce5f7bd88daf849ffe9ecd
EzekialPage <39393643+EzekialPage@users.noreply.github.com>
2021-02-11 08:43:01 -0600

    Update DCTScript2.ps1

--- DCTScript2.ps1
+++ DCTScript2.ps1
@@ -1,29 +1,77 @@
 Write-Output "Second part:"
-    $clientName = Read-Host -Prompt "Enter client name (ex. TESTCLIENT) "
-    $clientLOBName = Read-Host -Prompt "Enter client's LOB folder (ex. property) "
-    $clientPath = 'C:\SaaS\' + $clientName + '\Policy\ManuScripts\DCTTemplates\' + $clientLOBName
-    try {
-        $max = ""
-        $maxPath = ""
-        Get-ChildItem -Path ($clientPath) | ForEach-Object {
-            $stateName = $_.Name
-            Get-ChildItem -Path $_.FullName | ForEach-Object {
-                if ($max -lt $_.Name) {
-                    $maxPath = $_.FullName
-                    $fName = $_.Name
-                }
-            }
-            $encodingLine = Get-ChildItem -Path $maxPath -ErrorAction stop | Select-String -Pattern 'encoding="utf-8"'
-            $noEncodingLine = Get-ChildItem -Path $maxPath -ErrorAction stop | Where-Object {!($_ | Select-String 'encoding="utf-8' -Quiet) }
-            if ($encodingLine -eq $null) {
-                Write-Host "No utf-8 encoding lines found in " ($noEncodingLine | foreach {$stateName, $fName})
-            } else {
-                Write-Host "**********************"
-                Write-Host "Utf-8 encoding line found in the following files " ($encodingLine | foreach {$stateName, $fName})
-                Write-Host "**********************"
+#This script will iterate through a file strucutre and verify that 
+#the encoding line was removed from the top of the xml file 
+
+$clientName = Read-Host -Prompt "Enter client name (ex. TESTCLIENT) "
+$clientLOBName = Read-Host -Prompt "Enter client's LOB folder (ex. property) "
+$clientPath = 'C:\SaaS\' + $clientName + '\Policy\ManuScripts\DCTTemplates\' + $clientLOBName
+#$clientPath ='C:\Users\ebpag\Desktop\DuckCreek\' + $clientName + '\'+ $clientLOBName 
+
+#Determine the newest version number
+#function newestPath($clPath){
+#    $newestPath = ""
+#    Get-ChildItem -Path $clPath | ForEach{
+#        if($_.Name -gt $newestPath){
+#            $newestPath = $_.FullName
+#        }
+#    }
+#    return $newestPath
+#}
+
+#Determine the newest version number
+function newestVersion($clPath){
+    $newestVer = ""
+    #Iterate through child folder
+    Get-ChildItem -Path $clPath | ForEach{
+        #separate version number from title
+        $splitArr = ""
+        $splitArr = $_.BaseName.Split("_") 
+        $version = ""
+        for(($i = 0); ($i -lt $splitArr.Length);$i++){
+            if($splitArr[$i] -match "^\d+$"){
+                $version += "_" + $splitArr[$i]
             }
         }
-    } catch [System.Exception] {
-        Write-Host "An error occured "
-        Write-Host $_
+        #Find largest file version 
+        if($newestVer -lt $version){
+            $newestVer = $version
+        }
     }
+    return $newestVer
+}
+
+$flag = 0
+#Iterate through folders in LOB    
+Get-ChildItem -Path $clientPath | ForEach{
+    $LobPath = $_.FullName
+    $stateName = $_.Name
+    $outString = "Folder: " + $stateName + "`n"
+    $versionNum = newestVersion($LobPath)
+    $ifFlag = 0
+    Get-ChildItem -Path $LobPath | ForEach{ 
+        if($_.Name -Match $versionNum){
+            #determine if encoding line exists
+            $encodingLine = Get-ChildItem -Path $_.FullName -ErrorAction stop | Select-String -Pattern 'encoding="utf-8"'
+
+            #display results to user
+            if ($encodingLine -ne $null) {
+
+                #check for previous errors
+                if($flag -eq 0){
+                    Write-Host "Utf-8 encoding line found in the following files:" -ForegroundColor Red
+                    $flag++
+                }
+                $fileName = $_.Name
+                $outString += $fileName + "`n"
+                $ifFlag++
+            }
+        }#end if
+    }
+    if($ifFlag -ne 0){
+        Write-Host $outString
+    }
+}
+#check to see if there were no errors
+if($flag -eq 0){
+        Write-Host "No Utf-8 encoding lines were found" -ForegroundColor Green
+}
