d0e353c04a8509f46bbfa20c7c9cfa068ff2888a
EzekialPage <39393643+EzekialPage@users.noreply.github.com>
2021-02-10 20:33:35 -0600

    Update ManuPart4.ps1

--- ManuPart4.ps1
+++ ManuPart4.ps1
@@ -1,12 +1,14 @@
 Write-Host "Fourth part:"
-#lines about checking if table is present
+#This script will iterate through a file structure, find the newest version 
+#of a product file (if any) and verify manuscripts table values
+
+#Prompt user input and create pathing variables
+$clientName = Read-Host -Prompt "Enter client name to verfiy there is a table or not (ex. TESTCLIENT)"
 $clientLOB = Read-Host -Prompt "Enter LOB "
 $tableLOB = '<table id="Manuscripts' + $clientLOB
-$clientName = Read-Host -Prompt "Enter client name to verfiy there is a table or not (ex. Five)"
 $version = Read-Host -Prompt "Enter version Number (ex. 10_X_X_X)"
 $clientPath = "C:\Users\ebpag\Desktop\DuckCreek\$clientName\$clientLOB"
 $fileName = "*" +$clientLOB + "_Product_*.xml"
-$ifTrue = "False"
 
 #Determine path of newest version
 function newestPath($clPath){
@@ -18,9 +20,10 @@ function newestPath($clPath){
     }
     return $newestPath
 }
-
+#iterate through client folder
 Get-ChildItem -Path $clientPath | ForEach{
-    Write-Host $_.Name
+    #store output variables
+    $ifTrue = "False"
     $old = $_.FullName+"\$fileName"
     $newPath = newestPath($old)
 
@@ -38,7 +41,7 @@ Get-ChildItem -Path $clientPath | ForEach{
                 if($tableName -eq $targetName){
                     #stop and set flag true if valid version numbers found in table
                     $compareString = $empDetail.data.row.value
-                    if ($compareString -Match $version ) {
+                    if ($compareString -match $version ) {
                         $ifTrue = "True"
                         break
                     }
@@ -46,17 +49,23 @@ Get-ChildItem -Path $clientPath | ForEach{
             }
             #output results of comparison
             if ($ifTrue -eq "True") {
-                Write-Host "Manuscript table was successfully updated"
-                Write-Host " "
+                #Write-Host "Manuscript table was successfully updated" -ForegroundColor Green
+                #Write-Host ""
             } else {
+                Write-Host "Folder: " $_.Name
                 Write-Host "Manuscript table failed to update successfully" -ForegroundColor Red
-                Write-Host " "
+                Write-Host ""
             }
         } else {
+            #no table was found in product file
+            Write-Host "Folder: " $_.Name
             Write-Host "No table present" -ForegroundColor Red
+            Write-Host ""
         }
     }else{
-        #no product file found
+        #no product file was found in folder
+        Write-Host "Folder: " $_.Name
         Write-Host "No product file found" -ForegroundColor Red
+        Write-Host ""
     }
 }
