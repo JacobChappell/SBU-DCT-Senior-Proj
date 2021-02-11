48911c28389dd9640ee93bda8c31d264da1d6ec2
EzekialPage <39393643+EzekialPage@users.noreply.github.com>
2021-02-11 08:43:50 -0600

    Update DCTScript4.ps1

--- DCTScript4.ps1
+++ DCTScript4.ps1
@@ -1,57 +1,71 @@
-Write-Output "Fourth part:"
-#lines about checking if table is present
-$clientLOB = Read-Host -Prompt "Enter LOB (ex. DuckCreekTech) "
-$clientName = Read-Host -Prompt "Enter client name (ex. TESTCLIENT) "
-$clientLOBName = Read-Host -Prompt "Enter client's LOB folder (ex. property) "
-$clientPath = 'C:\SaaS\' + $clientName + '\Policy\ManuScripts\DCTTemplates\' + $clientLOBName
-$fileName = $clientLOB + "_Product*"
-$versionNum = Read-Host -Prompt "Enter version number (ex. 00_41) "
+Write-Host "Fourth part:"
+#This script will iterate through a file structure, find the newest version 
+#of a product file (if any) and verify manuscripts table values
 
+#Prompt user input and create pathing variables
+$clientName = Read-Host -Prompt "Enter client name to verfiy there is a table or not (ex. TESTCLIENT)"
+$clientLOB = Read-Host -Prompt "Enter LOB "
+$tableLOB = '<table id="Manuscripts' + $clientLOB
+$version = Read-Host -Prompt "Enter version Number (ex. 10_X_X_X)"
+$clientPath = "C:\Users\ebpag\Desktop\DuckCreek\$clientName\$clientLOB"
+$fileName = "*" +$clientLOB + "_Product_*.xml"
+
+#Determine path of newest version
 function newestPath($clPath){
     $newestPath = ""
     Get-ChildItem -Path $clPath | ForEach{
-        if ($_.Name -gt $newestPath){
+        if($_.Name -gt $newestPath){
             $newestPath = $_.FullName
         }
     }
-    Write-Host $newestPath
     return $newestPath
 }
-
+#iterate through client folder
 Get-ChildItem -Path $clientPath | ForEach{
+    #store output variables
+    $ifTrue = "False"
     $old = $_.FullName+"\$fileName"
     $newPath = newestPath($old)
-    if ($newPath -eq ""){
-        Write-Host "**********************"
-        Write-Host "No valid version found in $_"
-        Write-Host "**********************"
-    } else{
+
+    #check for valid product file
+    if($newPath -ne ""){
+        #check for valid table
         $tableLine = Get-ChildItem -Path $newPath | Select-String -Pattern $tableLOB -Quiet
         if ($tableLine) {
-            $compareString = ""
-            $ifTrue = "False"
             [XML]$tableData = Get-Content $newPath
-            foreach($empDetail in $tableData.table.data.row) {
-                $compareString = $empDetail.value
-                if ($empDetail.value.Contains($versionNum)) {
-                    $ifTrue = "True"
-                    break
+            #iterate through xml file
+            foreach($empDetail in $tableData.manuscript.model.object.table) {
+                $tableName = $empDetail.id
+                $targetName = "Manuscripts"+$clientLOB
+                #look for table with correct name
+                if($tableName -eq $targetName){
+                    #stop and set flag true if valid version numbers found in table
+                    $compareString = $empDetail.data.row.value
+                    if ($compareString -match $version ) {
+                        $ifTrue = "True"
+                        break
+                    }
                 }
             }
+            #output results of comparison
             if ($ifTrue -eq "True") {
-                Write-Output $_.Name
-                Write-Host "Manuscript table was successfully updated"
+                #Write-Host "Manuscript table was successfully updated" -ForegroundColor Green
+                #Write-Host ""
             } else {
-                Write-Host "**********************"
-                Write-Output $_.Name
-                Write-Host "Manuscript table failed to update successfully"
-                Write-Host "**********************"
+                Write-Host "Folder: " $_.Name
+                Write-Host "Manuscript table failed to update successfully" -ForegroundColor Red
+                Write-Host ""
             }
         } else {
-            Write-Host "**********************"
-            Write-Output $_.Name
-            Write-Host "No table present"
-            Write-Host "**********************"
+            #no table was found in product file
+            Write-Host "Folder: " $_.Name
+            Write-Host "No table present" -ForegroundColor Red
+            Write-Host ""
         }
+    }else{
+        #no product file was found in folder
+        Write-Host "Folder: " $_.Name
+        Write-Host "No product file found" -ForegroundColor Red
+        Write-Host ""
     }
 }
