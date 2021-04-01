761d14e71c9a427a4ddd43b5ba2d0e887dfd123c
MaximillianStoner <37821232+MaximillianStoner@users.noreply.github.com>
2021-04-01 18:57:06 -0500

    Changed pathing

--- CheckEffectiveDate.ps1
+++ CheckEffectiveDate.ps1
@@ -1,43 +1,61 @@
-﻿
-        
-
+            #Effective Date Checker
+            #Updated by Max Stoner 4/1/21
+            #This code imports two effective dates and compares them:
+            #If the first date is later than the second date, the code will succeed!
+            
+            
+            
+            #Asks for LOB input
             $LOB = Read-Host -Prompt 'Input LOB as state abbreviation'
 
+            #Writes LOB (REMOVE AFTER DEBUGGING)
             Write-Output $LOB
 
-            $path = "C:\Users\colby.welch\Desktop\Test\DCTTemplate\$LOB\EXAMPLE1.0.xml"
-            $path2 = "C:\Users\colby.welch\Desktop\Test\DCTTemplate\$LOB\EXAMPLE1.1.xml"
-
+            #Path creation
+            $path = "C:\Program Files\Duck Creek Technologies_MO_Base_1905\ManuScripts\DCTTemplates\$LOB\*\BaseProduct.xml"
+            $path2 = "C:\Program Files\Duck Creek Technologies_MO_Base_1905\ManuScripts\DCTTemplates\$LOB\*\BillingBaseProduct.xml"
+            
+            #Import the xml information as a File
             if (Test-Path $path){
-                [xml]$File = Get-Content $path 
+                [xml]$File = Get-Content $path
                 [xml]$File2 = Get-Content $path2
             }
-                
+            #Error: Unless there is no file at path 
             else{
                 Write-Error "Path Check Failed in file " $LOB
             }
-                
+            
+            #If the files aren't blank, pull out EffectiveDateNew and cast it as a date
             if ( $File -ne $null -and $File2 -ne $null){
-                $date = $File.SelectSingleNode('//effectiveDateNew').'#text' -as [datetime]
-                $date2 = $File2.SelectSingleNode('//effectiveDateNew').'#text' -as [datetime]
+                $date = $File.SelectSingleNode('//@effectiveDateNew').'#text' -as [datetime]
+                $date2 = $File2.SelectSingleNode('//@effectiveDateNew').'#text' -as [datetime]
 
+                #Output dates (REMOVE AFTER DEBUGGING)
                 Write-Output $date
                 Write-Output $date2
 
+                #if the first file's date is less than file 2's it's valid
                 if ($date -lt $date2){
                     Write-Host -ForegroundColor Green "Effective Date Valid in file $LOB" `n
                 }
 
+                #if date is empty (REMOVE AFTER DEBUGGING)
+                if($date -eq $null -or $date2 -eq $null){
+                    Write-Host -ForegroundColor Red "Big Fart time" `n
+                }
+
+                #else invalid
                 else{
                     Write-Host -ForegroundColor Red "Invaild Effective Date in file $LOB" `n
                 }
 
             }
-                    
+            
+            #else the files are blank    
             else{
                 Write-Error "Invalid File"
             }
 
+            #cleaning variables
             $File = $null
-            $File2 = $null
-        
+            $File2 = $null 
